8e9e5c9096049f14d710385a4c6aea328ce855b6
Piyush Sharma <34617552+07piyush@users.noreply.github.com>
2020-04-22 19:38:16 +0530

    Update LinkLayerInformation.h

--- LinkLayerInformation.h
+++ LinkLayerInformation.h
@@ -7,9 +7,10 @@ using namespace std;
 
 struct LinkLayerInformation{
 	
-	 unsigned char destination[6],
+	unsigned char destination[6],
 		       source[6];
 	unsigned short connectionType;
+	bool nextIsIP;
 
 	LinkLayerInformation() {
 		source[0] = '\0';
@@ -17,6 +18,29 @@ struct LinkLayerInformation{
 		connectionType = 0;
 	 }
 	
+	void getSourceAddressFormated(unsigned char result[]){
+		unsigned char value;
+		int index = 0;
+		for(int byte = 0; byte < 5; byte++){
+			value = source[byte];
+			index += snprintf((char *)result+index, 4, "%x:", value);
+		}
+		value = source[5];
+		index += snprintf((char *)result+index, 4, "%x", value);
+	}
+	
+	void getDestinationAddressFormated(unsigned char result[]){
+		unsigned char value;
+		int index = 0;
+		for(int byte = 0; byte < 5; byte++){
+			value = destination[byte];
+			index += snprintf((char *)result+index, 4, "%x:", value);
+		}
+		value = destination[5];
+		index += snprintf((char *)result+index, 4, "%x", value);
+	}
+
+
 	~LinkLayerInformation() { }
     
 };
