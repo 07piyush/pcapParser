b54b24a46e3a04914eff25659fc5d86efa2eebdf
Piyush Sharma <34617552+07piyush@users.noreply.github.com>
2020-04-22 19:41:46 +0530

    Update TransportLayerInformation.h

--- TransportLayerInformation.h
+++ TransportLayerInformation.h
@@ -1,3 +1,13 @@
+/**********************************************************************************
+
+* This file is part of c++ assignment 2 and assignment 3.
+* This file is declaration of structure "TransportLayerInformation".
+* This structure is used to store information that are retrieved from pcap file at transport layer level.
+* Objective of this structure is to provide abstract form store port addresses and define protocol for this layer.
+* also to derived information so as to write into a .csv file.
+
+*******************************************************************************/
+
 #ifndef TransportLayerInformation_H
 #define TransportLayerInformation_H
 
@@ -9,8 +19,20 @@ enum protocolType {TCP, UDP, OTHER};
 
 struct TransportLayerInformation{
 	
-	protocolType protocol;
-	unsigned short sourcePort, destinationPort;
+	protocolType protocol;		// can be used as filter tap to read or ignore information from this structure.
+	unsigned short sourcePort, destinationPort;	// contains port addresses if protol is TCP or UDP else contains garbage value.
+	
+//The below two funtions write addresses into result array, to write into file.
+	void getFormatedSource(unsigned char result[])
+	{
+
+		snprintf((char *)result, 6, "%d", sourcePort);
+	}
+
+	void getFormatedDestination(unsigned char result[])
+	{
+		snprintf((char *)result, 6, "%d", destinationPort);
+	}
 
 	TransportLayerInformation() { }
 };
